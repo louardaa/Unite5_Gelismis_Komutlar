5689d30546b4a50d6d3a8f319622ad8c5d5d16f2
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-02-22 12:14:29 +0300

    no message

---
+++ app/build/generated/source/buildConfig/debug/com/example/unite5_gelismis_komutlar/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.unite5_gelismis_komutlar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.unite5_gelismis_komutlar";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
